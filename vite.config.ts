c977973e7006fe1e59cde05bb5c9c517c90fbd20
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-06 19:42:42 +0300

    version 0.0.3

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: "src/koi-table.vue",
+      name: "KoiTable",
+      fileName: (format) => `koi-table.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});

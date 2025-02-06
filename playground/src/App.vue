fc81c92dc38dcaa6c60170019125c061ab7d6854
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-06 19:00:52 +0300

    version 0.0.2

---
+++ playground/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import KoijsTable from '../../src/koi-table.vue'
+
+const columns = ["ID", "Name", "Age"];
+const data = [
+  { ID: 1, Name: "Ali", Age: 25 },
+  { ID: 2, Name: "Veli", Age: 30 },
+];
+</script>
+
+
+<template>
+  <div>
+    <h1>Koijs Table Test</h1>
+    <KoijsTable :columns="columns" :data="data" />
+  </div>
+</template>
+
---
+++ src/koi-table.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="table-container">
+    <table class="custom-table">
+      <thead>
+        <tr>
+          <th v-for="(col, index) in props.columns" :key="index" class="animated-header">
+            {{ col }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(row, index) in props.data"
+          :key="index"
+          class="animated-row"
+        >
+          <td v-for="col in props.columns" :key="col">{{ row[col] || "-" }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  columns: [];
+  data: [];
+}>();
+</script>
+
+<style>
+.table-container {
+  overflow-x: auto;
+}
+
+.custom-table {
+  width: 100%;
+  border-collapse: collapse;
+  text-align: left;
+  font-family: Arial, sans-serif;
+  transition: transform 0.3s ease-in-out;
+}
+
+.custom-table th,
+.custom-table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  position: relative;
+}
+
+.custom-table th {
+  background-color: #f4f4f4;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.2s ease;
+}
+
+.custom-table th:hover {
+  background-color: #e0e0e0;
+  transform: translateY(-2px);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.custom-table tr {
+  transition: background-color 0.3s ease;
+}
+
+.custom-table tr:nth-child(even) {
+  background-color: #fafafa;
+}
+
+.custom-table tr:hover {
+  background-color: #f0f0f0;
+}
+
+.animated-row {
+  animation: fadeIn 0.5s ease;
+}
+
+.animated-header {
+  animation: fadeInHeader 0.7s ease-in-out;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes fadeInHeader {
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>

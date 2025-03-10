581e7a3b2858941a0253cf80ec921a373215350a
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-03-10 23:50:05 +0300

    playground cleaned!

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,42 +1,8 @@
 <script setup lang="ts">
-import type { Column, Row } from '@koijs/table-vue/types'
-import KoiTable from '@koijs/table-vue'
-import { onMounted, ref } from 'vue'
-import Default from './layouts/default.vue'
-// import '@koijs/table-vue/ui'
-
-const rows = ref<Row[]>([])
-const columns = ref<Column[]>([])
-columns.value = []
-
-onMounted(async () => {
-  await fetch('https://dummyjson.com/todos')
-    .then(res => res.json())
-    .then((data) => {
-      rows.value = data.todos
-    })
-})
 </script>
 
 <template>
-  <Default>
-    <div class="w-full h-full flex justify-center items-center">
-      <div class="w-8/12 h-4/6 flex">
-        <KoiTable
-          :rows="rows ?? []"
-          size="sm"
-          :zebra-rows="true"
-          :border="{
-            horizontal: false,
-          }"
-          class="whitespace-nowrap"
-          :spacing="false"
-        >
-          <template #image-cell="{ data }">
-            <img :src="data.image">
-          </template>
-        </KoiTable>
-      </div>
-    </div>
-  </Default>
+  <div class="flex">
+    GO KOI!
+  </div>
 </template>
